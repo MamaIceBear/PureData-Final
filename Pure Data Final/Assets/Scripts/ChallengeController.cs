b1be5ae25a79e5a2c68a21331a89e7fdf8897e5e
MamaIceBear <snguye39@ucsc.edu>
2021-03-03 10:00:43 -0800

    Challenges

---
+++ Pure Data Final/Assets/Scripts/ChallengeController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChallengeController : MonoBehaviour
+{
+    public float scrollSpeed = 5.0f;
+    public GameObject[] challenges; 
+    public float frequency = 0.5f;
+    float counter = 0.0f;
+    public Transform challengesSpawnPoint;
+
+    void Start()
+    {
+        GenerateRandomChallenge();
+    }
+
+    void Update()
+    {
+        //Generate Objects
+        if(counter <= 0f)
+        {
+            GenerateRandomChallenge();
+        }
+        else
+        {
+            counter -= Time.deltaTime * frequency;
+        }
+
+        //Scrolling
+        GameObject currentChild;
+        for(int i = 0; i < transform.childCount; i++)
+        {
+            currentChild = transform.GetChild(i).gameObject;
+            ScrollChallenge(currentChild);
+            if(currentChild.transform.position.x <= -15.0f)
+            {
+                Destroy(currentChild);
+            }
+        }
+    }
+
+    void ScrollChallenge(GameObject currentChallenge)
+    {
+        currentChallenge.transform.position -= Vector3.right * (scrollSpeed * Time.deltaTime);
+    }
+
+    void GenerateRandomChallenge()
+    {
+        GameObject newChallenge = Instantiate(challenges[Random.Range(0, challenges.Length)], challengesSpawnPoint.transform);
+        newChallenge.transform.parent = transform;
+        counter++;
+    }
+}
---
+++ Pure Data Final/Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    public float jumpPower = 10f;
+    Rigidbody2D myRigidbody;
+    bool isGrounded = false;
+
+    void Start()
+    {
+       myRigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate() 
+    {
+        if(Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            myRigidbody.AddForce(Vector3.up * (jumpPower * myRigidbody.mass * myRigidbody.gravityScale * 20.0f));
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D other) 
+    {
+        if(other.collider.tag == "Ground")
+        {
+            isGrounded = true;
+        }
+    }
+
+    void OnCollisionStay2D(Collision2D other) 
+    {
+        if(other.collider.tag == "Ground")
+        {
+            isGrounded = true;
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D other) 
+    {
+        if(other.collider.tag == "Ground")
+        {
+            isGrounded = false;
+        }
+    }
+}
