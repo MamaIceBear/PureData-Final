141287b8f02157c0a585ad4de959eab01b734c95
MamaIceBear <snguye39@ucsc.edu>
2021-03-03 07:15:05 -0800

    Cube setup

--- Pure Data Final/Assets/Scripts/Jump.cs
+++ Pure Data Final/Assets/Scripts/Jump.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class Jump : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private Rigidbody rb;
+
     void Start()
     {
-        rb.GetComponent<Rigidbody>();
+        rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
     void Update()
     {
+        Vector3 vel = rb.velocity;
+        vel.x = 5;
+        rb.velocity = vel;
         if(Physics.Raycast(transform.position, Vector3.down, GetComponent<BoxCollider>().size.y / 2 + 0.4f))
         {
             Quaternion rot = transform.rotation;
@@ -21,7 +24,7 @@ public class Jump : MonoBehaviour
             if(Input.GetMouseButtonDown(0))
             {
                 rb.velocity = Vector3.zero;
-                rb.AddForce(Vector2.up * 55000)
+                rb.AddForce(Vector2.up * 55000);
             }
         }
         else
