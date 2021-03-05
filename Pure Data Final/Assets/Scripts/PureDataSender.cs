31fd2e55c77a2a56acb96781f68cf1b7c166ea6d
MamaIceBear <snguye39@ucsc.edu>
2021-03-04 16:28:27 -0800

    PureData Sender

---
+++ Pure Data Final/Assets/Scripts/PureDataSender.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PureDataSender : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        OSCHandler.Instance.Init();
+        OSCHandler.Instance.SendMessageToClient("pd", "/unity/ready", "CONNECTION TO UNITY SUCCESSFUL!");
+        OSCHandler.Instance.SendMessageToClient("pd", "/unity/distance/enable", 1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
