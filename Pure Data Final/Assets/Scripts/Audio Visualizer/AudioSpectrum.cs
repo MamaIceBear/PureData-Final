6feb038820da67643a4a55e5ae1070ecdc5588ce
Nathan Ma <ndma@ucsc.edu>
2021-03-11 21:37:56 -0800

    New spectrumdata audio reactor

---
+++ Pure Data Final/Assets/Scripts/Audio Visualizer/AudioSpectrum.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioSpectrum : MonoBehaviour {
+
+    public static float spectrumValue {get; private set;}
+    
+    private float[] m_audioSpectrum;
+
+    private void Start()
+    {
+        /// initialize buffer
+        m_audioSpectrum = new float[128];
+    }
+
+	private void Update()
+    {
+        // get the data
+        AudioListener.GetSpectrumData(m_audioSpectrum, 0, FFTWindow.Hamming);
+
+        // assign spectrum value
+        // this "engine" focuses on the simplicity of other classes only..
+        // ..needing to retrieve one value (spectrumValue)
+        if (m_audioSpectrum != null && m_audioSpectrum.Length > 0)
+        {
+            spectrumValue = m_audioSpectrum[0] * 100;
+        }
+    }
+
+}
