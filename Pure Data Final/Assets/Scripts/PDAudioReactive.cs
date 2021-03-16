90f7da7a80a088eaa2ad6966c3ac3bb2bdd731a3
Nathan Ma <ndma@ucsc.edu>
2021-03-15 17:13:38 -0700

    Update colors and frequency ranges

--- Pure Data Final/Assets/Scripts/PDAudioReactive.cs
+++ Pure Data Final/Assets/Scripts/PDAudioReactive.cs
@@ -17,6 +17,7 @@ public class PDAudioReactive : MonoBehaviour
     public GameObject sprite;
 
     public Color restColor = new Color (0f, 0f, 0f, 0f);
+    public Color beatColor;
     public float TimeToRest = 5f;
     
     private Image m_img;
@@ -36,30 +37,19 @@ public class PDAudioReactive : MonoBehaviour
         {
             if(item.Value.log.Count > 0)
             {
-                Debug.Log(item.Value.packets[0].Address);
-                Debug.Log(item.Value.packets[0].Data[0].ToString());
                 if (item.Value.packets[0].Address == "/melodyFreq"){
+                    Debug.Log(item.Value.packets[0].Address);
+                    Debug.Log(item.Value.packets[0].Data[0].ToString());
                     frequency = float.Parse(item.Value.packets[0].Data[0].ToString());
                 }
             }
         }
 
         if (minFreq <= frequency && frequency < maxFreq){
-            m_img.color = Color.Lerp(m_img.color, freqToColor(), TimeToRest * Time.deltaTime);
+            m_img.color = Color.Lerp(m_img.color, beatColor, TimeToRest * Time.deltaTime);
         }
         else{
             m_img.color = Color.Lerp(m_img.color, restColor, TimeToRest * Time.deltaTime);
         }
-    }
-
-    private Color freqToColor(){
-        if (frequency < 33f){return new Color (148f, 0f, 211f, 255f);}
-        else if (frequency < 66f){return new Color (75f, 0f, 130f, 255f);}
-        else if (frequency < 131f){return new Color (0f, 0f, 255f, 255f);}
-        else if (frequency < 262f){return new Color (0f, 255f, 0f, 255f);}
-        else if (frequency < 523f){return new Color (255f, 255f, 0f, 255f);}
-        else if (frequency < 1047f){return new Color (255f, 127f, 0f, 255f);}
-        else if (frequency < 2093f){return new Color (255f, 0f, 0f, 255f);}
-        else {return new Color (255f, 255f, 255f, 255f);}
     }
 }
